25c9243e1000cf7c92931f8782f07ad4314452af
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-03-04 02:53:57 +0000

    Vote update

--- trunk/votes/dc_dem.js
+++ trunk/votes/dc_dem.js
@@ -1,1 +1,12 @@
-GoogleElectionMap.votesReady({ "status":"none", "party":"dem", "state":"dc" })
+GoogleElectionMap.votesReady({"status":"ok","totals":
+{"date":"02-12","votes":[
+{"votes":85534,"name":"obama"},
+{"votes":27326,"name":"clinton"},
+{"votes":285,"name":"edwards"},
+{"votes":172,"name":"kucinich"},
+{"votes":132,"name":"richardson"}],
+"precincts":
+{"total":142,"reporting":139},
+"name":"District of Columbia"},
+"state":"DC","party":"dem","total":113449,"locals":
+{}})

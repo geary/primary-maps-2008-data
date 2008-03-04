fdd26141135ee71ee0b2b8f2c3444402ac135614
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-03-04 02:53:57 +0000

    Vote update

--- votes/dc_gop.js
+++ votes/dc_gop.js
@@ -1,1 +1,12 @@
-GoogleElectionMap.votesReady({ "status":"none", "party":"gop", "state":"dc" })
+GoogleElectionMap.votesReady({"status":"ok","totals":
+{"date":"02-12","votes":[
+{"votes":3929,"name":"mccain"},
+{"votes":961,"name":"huckabee"},
+{"votes":471,"name":"paul"},
+{"votes":350,"name":"romney"},
+{"votes":90,"name":"giuliani"}],
+"precincts":
+{"total":142,"reporting":139},
+"name":"District of Columbia"},
+"state":"DC","party":"gop","total":5801,"locals":
+{}})

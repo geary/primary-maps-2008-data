fb0ae7e9562849aac8b6072a2e160d11dfe7ad5c
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-03-05 03:58:59 +0000

    Vote update

--- trunk/votes/nv_gop.js
+++ trunk/votes/nv_gop.js
@@ -23,163 +23,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "total":1988,"precincts":
 {"total":26,"reporting":26},
 "name":"Carson City"},
-"Esmeralda County":
-{"votes":[
-{"votes":18,"name":"romney"},
-{"votes":15,"name":"paul"},
-{"votes":9,"name":"mccain"},
-{"votes":7,"name":"huckabee"},
-{"votes":6,"name":"giuliani"},
-{"votes":3,"name":"thompson"},
-{"votes":2,"name":"hunter"}],
-"total":60,"precincts":
-{"total":5,"reporting":5},
-"name":"Esmeralda County"},
-"Humboldt County":
-{"votes":[
-{"votes":193,"name":"romney"},
-{"votes":117,"name":"mccain"},
-{"votes":104,"name":"huckabee"},
-{"votes":76,"name":"paul"},
-{"votes":36,"name":"thompson"},
-{"votes":10,"name":"giuliani"},
-{"votes":7,"name":"hunter"}],
-"total":543,"precincts":
-{"total":15,"reporting":15},
-"name":"Humboldt County"},
-"Elko County":
-{"votes":[
-{"votes":717,"name":"romney"},
-{"votes":212,"name":"paul"},
-{"votes":166,"name":"mccain"},
-{"votes":129,"name":"huckabee"},
-{"votes":106,"name":"thompson"},
-{"votes":30,"name":"giuliani"},
-{"votes":9,"name":"hunter"}],
-"total":1369,"precincts":
-{"total":41,"reporting":41},
-"name":"Elko County"},
-"Lincoln County":
-{"votes":[
-{"votes":208,"name":"romney"},
-{"votes":17,"name":"mccain"},
-{"votes":13,"name":"huckabee"},
-{"votes":12,"name":"paul"},
-{"votes":10,"name":"thompson"},
-{"votes":0,"name":"hunter"},
-{"votes":0,"name":"giuliani"}],
-"total":260,"precincts":
-{"total":5,"reporting":5},
-"name":"Lincoln County"},
-"Eureka County":
-{"votes":[
-{"votes":24,"name":"romney"},
-{"votes":18,"name":"mccain"},
-{"votes":16,"name":"paul"},
-{"votes":15,"name":"huckabee"},
-{"votes":7,"name":"thompson"},
-{"votes":6,"name":"giuliani"},
-{"votes":0,"name":"hunter"}],
-"total":86,"precincts":
-{"total":4,"reporting":4},
-"name":"Eureka County"},
-"Lander County":
-{"votes":[
-{"votes":86,"name":"romney"},
-{"votes":42,"name":"huckabee"},
-{"votes":40,"name":"mccain"},
-{"votes":39,"name":"paul"},
-{"votes":20,"name":"thompson"},
-{"votes":14,"name":"giuliani"},
-{"votes":4,"name":"hunter"}],
-"total":245,"precincts":
-{"total":9,"reporting":9},
-"name":"Lander County"},
-"Mineral County":
-{"votes":[
-{"votes":38,"name":"romney"},
-{"votes":31,"name":"huckabee"},
-{"votes":17,"name":"mccain"},
-{"votes":12,"name":"thompson"},
-{"votes":8,"name":"paul"},
-{"votes":4,"name":"hunter"},
-{"votes":3,"name":"giuliani"}],
-"total":113,"precincts":
-{"total":12,"reporting":12},
-"name":"Mineral County"},
-"Douglas County":
-{"votes":[
-{"votes":935,"name":"romney"},
-{"votes":333,"name":"mccain"},
-{"votes":311,"name":"paul"},
-{"votes":236,"name":"thompson"},
-{"votes":190,"name":"huckabee"},
-{"votes":106,"name":"giuliani"},
-{"votes":102,"name":"hunter"}],
-"total":2213,"precincts":
-{"total":41,"reporting":41},
-"name":"Douglas County"},
-"Nye County":
-{"votes":[
-{"votes":415,"name":"paul"},
-{"votes":399,"name":"romney"},
-{"votes":172,"name":"huckabee"},
-{"votes":134,"name":"mccain"},
-{"votes":55,"name":"thompson"},
-{"votes":33,"name":"giuliani"},
-{"votes":18,"name":"hunter"}],
-"total":1226,"precincts":
-{"total":33,"reporting":33},
-"name":"Nye County"},
-"Churchill County":
-{"votes":[
-{"votes":400,"name":"romney"},
-{"votes":174,"name":"mccain"},
-{"votes":166,"name":"paul"},
-{"votes":114,"name":"huckabee"},
-{"votes":72,"name":"thompson"},
-{"votes":29,"name":"giuliani"},
-{"votes":23,"name":"hunter"}],
-"total":978,"precincts":
-{"total":19,"reporting":19},
-"name":"Churchill County"},
-"Storey County":
-{"votes":[
-{"votes":67,"name":"romney"},
-{"votes":55,"name":"paul"},
-{"votes":28,"name":"thompson"},
-{"votes":12,"name":"huckabee"},
-{"votes":10,"name":"hunter"},
-{"votes":10,"name":"giuliani"},
-{"votes":9,"name":"mccain"}],
-"total":191,"precincts":
-{"total":15,"reporting":15},
-"name":"Storey County"},
-"Lyon County":
-{"votes":[
-{"votes":705,"name":"romney"},
-{"votes":238,"name":"mccain"},
-{"votes":234,"name":"thompson"},
-{"votes":217,"name":"paul"},
-{"votes":165,"name":"huckabee"},
-{"votes":78,"name":"hunter"},
-{"votes":73,"name":"giuliani"}],
-"total":1710,"precincts":
-{"total":40,"reporting":40},
-"name":"Lyon County"},
-"Washoe County":
-{"votes":[
-{"votes":4032,"name":"romney"},
-{"votes":1429,"name":"mccain"},
-{"votes":1317,"name":"paul"},
-{"votes":784,"name":"thompson"},
-{"votes":635,"name":"huckabee"},
-{"votes":527,"name":"giuliani"},
-{"votes":344,"name":"hunter"}],
-"total":9068,"precincts":
-{"total":438,"reporting":438},
-"name":"Washoe County"},
-"White Pine County":
+"White Pine":
 {"votes":[
 {"votes":160,"name":"romney"},
 {"votes":25,"name":"mccain"},
@@ -190,8 +34,56 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":1,"name":"hunter"}],
 "total":233,"precincts":
 {"total":10,"reporting":10},
-"name":"White Pine County"},
-"Clark County":
+"name":"White Pine"},
+"Lincoln":
+{"votes":[
+{"votes":208,"name":"romney"},
+{"votes":17,"name":"mccain"},
+{"votes":13,"name":"huckabee"},
+{"votes":12,"name":"paul"},
+{"votes":10,"name":"thompson"},
+{"votes":0,"name":"hunter"},
+{"votes":0,"name":"giuliani"}],
+"total":260,"precincts":
+{"total":5,"reporting":5},
+"name":"Lincoln"},
+"Churchill":
+{"votes":[
+{"votes":400,"name":"romney"},
+{"votes":174,"name":"mccain"},
+{"votes":166,"name":"paul"},
+{"votes":114,"name":"huckabee"},
+{"votes":72,"name":"thompson"},
+{"votes":29,"name":"giuliani"},
+{"votes":23,"name":"hunter"}],
+"total":978,"precincts":
+{"total":19,"reporting":19},
+"name":"Churchill"},
+"Lander":
+{"votes":[
+{"votes":86,"name":"romney"},
+{"votes":42,"name":"huckabee"},
+{"votes":40,"name":"mccain"},
+{"votes":39,"name":"paul"},
+{"votes":20,"name":"thompson"},
+{"votes":14,"name":"giuliani"},
+{"votes":4,"name":"hunter"}],
+"total":245,"precincts":
+{"total":9,"reporting":9},
+"name":"Lander"},
+"Humboldt":
+{"votes":[
+{"votes":193,"name":"romney"},
+{"votes":117,"name":"mccain"},
+{"votes":104,"name":"huckabee"},
+{"votes":76,"name":"paul"},
+{"votes":36,"name":"thompson"},
+{"votes":10,"name":"giuliani"},
+{"votes":7,"name":"hunter"}],
+"total":543,"precincts":
+{"total":15,"reporting":15},
+"name":"Humboldt"},
+"Clark":
 {"votes":[
 {"votes":13841,"name":"romney"},
 {"votes":2842,"name":"paul"},
@@ -202,8 +94,104 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":217,"name":"hunter"}],
 "total":23844,"precincts":
 {"total":1078,"reporting":1078},
-"name":"Clark County"},
-"Pershing County":
+"name":"Clark"},
+"Esmeralda":
+{"votes":[
+{"votes":18,"name":"romney"},
+{"votes":15,"name":"paul"},
+{"votes":9,"name":"mccain"},
+{"votes":7,"name":"huckabee"},
+{"votes":6,"name":"giuliani"},
+{"votes":3,"name":"thompson"},
+{"votes":2,"name":"hunter"}],
+"total":60,"precincts":
+{"total":5,"reporting":5},
+"name":"Esmeralda"},
+"Lyon":
+{"votes":[
+{"votes":705,"name":"romney"},
+{"votes":238,"name":"mccain"},
+{"votes":234,"name":"thompson"},
+{"votes":217,"name":"paul"},
+{"votes":165,"name":"huckabee"},
+{"votes":78,"name":"hunter"},
+{"votes":73,"name":"giuliani"}],
+"total":1710,"precincts":
+{"total":40,"reporting":40},
+"name":"Lyon"},
+"Douglas":
+{"votes":[
+{"votes":935,"name":"romney"},
+{"votes":333,"name":"mccain"},
+{"votes":311,"name":"paul"},
+{"votes":236,"name":"thompson"},
+{"votes":190,"name":"huckabee"},
+{"votes":106,"name":"giuliani"},
+{"votes":102,"name":"hunter"}],
+"total":2213,"precincts":
+{"total":41,"reporting":41},
+"name":"Douglas"},
+"Elko":
+{"votes":[
+{"votes":717,"name":"romney"},
+{"votes":212,"name":"paul"},
+{"votes":166,"name":"mccain"},
+{"votes":129,"name":"huckabee"},
+{"votes":106,"name":"thompson"},
+{"votes":30,"name":"giuliani"},
+{"votes":9,"name":"hunter"}],
+"total":1369,"precincts":
+{"total":41,"reporting":41},
+"name":"Elko"},
+"Washoe":
+{"votes":[
+{"votes":4032,"name":"romney"},
+{"votes":1429,"name":"mccain"},
+{"votes":1317,"name":"paul"},
+{"votes":784,"name":"thompson"},
+{"votes":635,"name":"huckabee"},
+{"votes":527,"name":"giuliani"},
+{"votes":344,"name":"hunter"}],
+"total":9068,"precincts":
+{"total":438,"reporting":438},
+"name":"Washoe"},
+"Mineral":
+{"votes":[
+{"votes":38,"name":"romney"},
+{"votes":31,"name":"huckabee"},
+{"votes":17,"name":"mccain"},
+{"votes":12,"name":"thompson"},
+{"votes":8,"name":"paul"},
+{"votes":4,"name":"hunter"},
+{"votes":3,"name":"giuliani"}],
+"total":113,"precincts":
+{"total":12,"reporting":12},
+"name":"Mineral"},
+"Storey":
+{"votes":[
+{"votes":67,"name":"romney"},
+{"votes":55,"name":"paul"},
+{"votes":28,"name":"thompson"},
+{"votes":12,"name":"huckabee"},
+{"votes":10,"name":"hunter"},
+{"votes":10,"name":"giuliani"},
+{"votes":9,"name":"mccain"}],
+"total":191,"precincts":
+{"total":15,"reporting":15},
+"name":"Storey"},
+"Eureka":
+{"votes":[
+{"votes":24,"name":"romney"},
+{"votes":18,"name":"mccain"},
+{"votes":16,"name":"paul"},
+{"votes":15,"name":"huckabee"},
+{"votes":7,"name":"thompson"},
+{"votes":6,"name":"giuliani"},
+{"votes":0,"name":"hunter"}],
+"total":86,"precincts":
+{"total":4,"reporting":4},
+"name":"Eureka"},
+"Pershing":
 {"votes":[
 {"votes":63,"name":"romney"},
 {"votes":40,"name":"mccain"},
@@ -214,4 +202,16 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":4,"name":"giuliani"}],
 "total":197,"precincts":
 {"total":6,"reporting":6},
-"name":"Pershing County"}}})
+"name":"Pershing"},
+"Nye":
+{"votes":[
+{"votes":415,"name":"paul"},
+{"votes":399,"name":"romney"},
+{"votes":172,"name":"huckabee"},
+{"votes":134,"name":"mccain"},
+{"votes":55,"name":"thompson"},
+{"votes":33,"name":"giuliani"},
+{"votes":18,"name":"hunter"}],
+"total":1226,"precincts":
+{"total":33,"reporting":33},
+"name":"Nye"}}})

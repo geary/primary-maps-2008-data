a627b90d8b2aae7ca711e7a2a54e79273f8c50fd
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-04-23 01:08:37 +0000

    Vote update

--- votes/us_gop.js
+++ votes/us_gop.js
@@ -1,9 +1,9 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":7313183,"name":"mccain","delegates":1111},
+{"votes":7320359,"name":"mccain","delegates":1111},
 {"votes":4340073,"name":"romney","delegates":271},
-{"votes":3921231,"name":"huckabee","delegates":265},
-{"votes":807291,"name":"paul","delegates":14},
+{"votes":3922367,"name":"huckabee","delegates":265},
+{"votes":808713,"name":"paul","delegates":14},
 {"votes":567806,"name":"giuliani"},
 {"votes":285898,"name":"thompson"},
 {"votes":46574,"name":"uncommitted-r"},
@@ -19,7 +19,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":23,"name":"fendig"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"gop","total":17370381,"locals":
+"state":"US","party":"gop","total":17380115,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":113074,"name":"mccain"},
@@ -378,11 +378,11 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":23,"date":"02-05","type":"caucus"},
 "Pennsylvania":
 {"votes":[
-{"votes":5763,"name":"mccain"},
-{"votes":1148,"name":"paul"},
-{"votes":807,"name":"huckabee"}],
+{"votes":12939,"name":"mccain"},
+{"votes":2570,"name":"paul"},
+{"votes":1943,"name":"huckabee"}],
 "name":"Pennsylvania","precincts":
-{"total":9268,"reporting":284},
+{"total":9268,"reporting":562},
 "delegates":74,"date":"04-22"},
 "Florida":
 {"votes":[

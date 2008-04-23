92193a4c11fff2434953bf9daed16e7c881e2362
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-04-23 02:49:35 +0000

    Vote update

--- trunk/votes/us_dem.js
+++ trunk/votes/us_dem.js
@@ -1,7 +1,7 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":14362080,"name":"clinton","delegates":1296},
-{"votes":14325383,"name":"obama","delegates":1452},
+{"votes":14380668,"name":"clinton","delegates":1296},
+{"votes":14335575,"name":"obama","delegates":1452},
 {"votes":925967,"name":"edwards"},
 {"votes":257005,"name":"uncommitted-d"},
 {"votes":104610,"name":"richardson"},
@@ -11,7 +11,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":21748,"name":"gravel"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"dem","total":30213820,"locals":
+"state":"US","party":"dem","total":30242600,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":255809,"name":"obama","delegates":20},
@@ -348,10 +348,10 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":13,"date":"02-05","type":"caucus"},
 "Pennsylvania":
 {"votes":[
-{"votes":896941,"name":"clinton","delegates":49},
-{"votes":754489,"name":"obama","delegates":43}],
+{"votes":915529,"name":"clinton","delegates":49},
+{"votes":764681,"name":"obama","delegates":43}],
 "name":"Pennsylvania","precincts":
-{"total":9268,"reporting":7055},
+{"total":9268,"reporting":7203},
 "delegates":158,"date":"04-22"},
 "Florida":
 {"votes":[

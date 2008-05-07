4b01cef964c2781511bb46f5173b1efabe47d114
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-05-07 02:44:09 +0000

    Vote update

--- trunk/votes/us_gop.js
+++ trunk/votes/us_gop.js
@@ -1,15 +1,15 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":8511840,"name":"mccain","delegates":1111},
+{"votes":8520949,"name":"mccain","delegates":1111},
 {"votes":4358181,"name":"romney","delegates":271},
-{"votes":4103837,"name":"huckabee","delegates":265},
-{"votes":995088,"name":"paul","delegates":14},
+{"votes":4105318,"name":"huckabee","delegates":265},
+{"votes":996053,"name":"paul","delegates":14},
 {"votes":567806,"name":"giuliani"},
 {"votes":285898,"name":"thompson"},
-{"votes":51394,"name":"keyes"},
+{"votes":51850,"name":"keyes"},
 {"votes":46574,"name":"uncommitted-r"},
 {"votes":37805,"name":"hunter"},
-{"votes":17281,"name":"nopreference-r"},
+{"votes":17787,"name":"nopreference-r"},
 {"votes":7971,"name":"tancredo"},
 {"votes":998,"name":"cort"},
 {"votes":623,"name":"tran"},
@@ -20,7 +20,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":23,"name":"fendig"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"gop","total":18986460,"locals":
+"state":"US","party":"gop","total":18998977,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":113074,"name":"mccain"},
@@ -519,13 +519,13 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":19,"date":"02-09","type":"caucus"},
 "North Carolina":
 {"votes":[
-{"votes":325033,"name":"mccain"},
-{"votes":53732,"name":"huckabee"},
-{"votes":32174,"name":"paul"},
-{"votes":17281,"name":"nopreference-r"},
-{"votes":11630,"name":"keyes"}],
+{"votes":334142,"name":"mccain"},
+{"votes":55213,"name":"huckabee"},
+{"votes":33139,"name":"paul"},
+{"votes":17787,"name":"nopreference-r"},
+{"votes":12086,"name":"keyes"}],
 "name":"North Carolina","precincts":
-{"total":2817,"reporting":2323},
+{"total":2817,"reporting":2401},
 "delegates":69,"date":"05-06"},
 "District of Columbia":
 {"votes":[

1e70a97424ebeef7e6fe4e98fce2d7da0cbe9175
election.maps.2008 <election.maps.2008@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-05-14 02:19:35 +0000

    Vote update

--- trunk/votes/us_dem.js
+++ trunk/votes/us_dem.js
@@ -1,18 +1,18 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":16166063,"name":"obama","delegates":1581},
-{"votes":16143262,"name":"clinton","delegates":1420},
-{"votes":938927,"name":"edwards"},
+{"votes":16170049,"name":"obama","delegates":1581},
+{"votes":16152076,"name":"clinton","delegates":1420},
+{"votes":939894,"name":"edwards"},
 {"votes":257005,"name":"uncommitted-d"},
 {"votes":104610,"name":"richardson"},
 {"votes":102554,"name":"kucinich"},
 {"votes":81165,"name":"biden"},
-{"votes":35390,"name":"gravel"},
+{"votes":35433,"name":"gravel"},
 {"votes":33308,"name":"dodd"},
 {"votes":22722,"name":"nopreference-d"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"dem","total":33885006,"locals":
+"state":"US","party":"dem","total":33898816,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":255809,"name":"obama","delegates":20},
@@ -285,11 +285,11 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":93,"date":"02-05"},
 "West Virginia":
 {"votes":[
-{"votes":117137,"name":"clinton"},
-{"votes":50167,"name":"obama"},
-{"votes":12960,"name":"edwards"}],
+{"votes":125701,"name":"clinton"},
+{"votes":53814,"name":"obama"},
+{"votes":13927,"name":"edwards"}],
 "name":"West Virginia","precincts":
-{"total":1894,"reporting":925},
+{"total":1894,"reporting":1019},
 "delegates":28,"date":"05-13"},
 "South Carolina":
 {"votes":[
@@ -403,9 +403,9 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"name":"Nebraska","precincts":
 {"total":1665,"reporting":1665},
 "votes":[
-{"votes":15532,"name":"clinton","delegates":8},
-{"votes":14842,"name":"obama","delegates":16},
-{"votes":1156,"name":"gravel"},
+{"votes":15782,"name":"clinton","delegates":8},
+{"votes":15181,"name":"obama","delegates":16},
+{"votes":1199,"name":"gravel"},
 {"votes":99,"name":"uncommitted-d"}],
 "delegates":24,"date":"02-09","type":"caucus"},
 "Missouri":

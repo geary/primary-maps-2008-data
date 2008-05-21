34edbabd5e67fc7f3241b5e6f9e7af03cc69fa15
election.maps.2008 <election.maps.2008@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-05-21 03:48:10 +0000

    Vote update

--- trunk/votes/us_dem.js
+++ trunk/votes/us_dem.js
@@ -1,7 +1,7 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":16923637,"name":"clinton","delegates":1485},
-{"votes":16678328,"name":"obama","delegates":1636},
+{"votes":16928291,"name":"clinton","delegates":1485},
+{"votes":16688573,"name":"obama","delegates":1636},
 {"votes":966225,"name":"edwards"},
 {"votes":275034,"name":"uncommitted-d"},
 {"votes":104610,"name":"richardson"},
@@ -12,7 +12,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":22722,"name":"nopreference-d"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"dem","total":35225669,"locals":
+"state":"US","party":"dem","total":35240568,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":255809,"name":"obama","delegates":20},
@@ -234,10 +234,10 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":83,"date":"02-12"},
 "Oregon":
 {"votes":[
-{"votes":229899,"name":"obama","delegates":22},
-{"votes":171480,"name":"clinton","delegates":12}],
+{"votes":240144,"name":"obama","delegates":22},
+{"votes":176134,"name":"clinton","delegates":12}],
 "name":"Oregon","precincts":
-{"total":1000,"reporting":480},
+{"total":1000,"reporting":495},
 "delegates":52,"date":"05-20"},
 "Connecticut":
 {"votes":[

d92552a23759f9b0b1d96cdef6e927868f858e3a
election.maps.2008 <election.maps.2008@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-05-21 07:16:52 +0000

    Vote update

--- votes/us_gop.js
+++ votes/us_gop.js
@@ -1,9 +1,9 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":9181126,"name":"mccain","delegates":1225},
+{"votes":9181503,"name":"mccain","delegates":1225},
 {"votes":4373398,"name":"romney","delegates":271},
 {"votes":4143462,"name":"huckabee","delegates":273},
-{"votes":1084119,"name":"paul","delegates":19},
+{"votes":1084215,"name":"paul","delegates":19},
 {"votes":573763,"name":"giuliani"},
 {"votes":285898,"name":"thompson"},
 {"votes":57204,"name":"uncommitted-r"},
@@ -20,7 +20,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":23,"name":"fendig"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"gop","total":19825480,"locals":
+"state":"US","party":"gop","total":19825953,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":113074,"name":"mccain","delegates":36},
@@ -253,10 +253,10 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":60,"date":"02-12"},
 "Oregon":
 {"votes":[
-{"votes":238692,"name":"mccain"},
-{"votes":41016,"name":"paul"}],
+{"votes":239069,"name":"mccain"},
+{"votes":41112,"name":"paul"}],
 "name":"Oregon","precincts":
-{"total":1000,"reporting":781},
+{"total":1000,"reporting":783},
 "delegates":30,"date":"05-20"},
 "Connecticut":
 {"votes":[

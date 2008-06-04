143f7af31439963ca0651c92c0aa3c626fba42d3
election.maps.2008 <election.maps.2008@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-06-04 19:17:15 +0000

    Vote update

--- trunk/votes/ak_dem.js
+++ trunk/votes/ak_dem.js
@@ -2,8 +2,8 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"name":"Alaska","precincts":
 {"total":40,"reporting":39},
 "votes":[
-{"votes":302,"name":"obama","delegates":9},
-{"votes":103,"name":"clinton","delegates":4},
+{"votes":302,"name":"obama","delegates":10},
+{"votes":103,"name":"clinton","delegates":3},
 {"votes":1,"name":"uncommitted-d"}],
 "delegates":13,"date":"02-05","type":"caucus"},
 "state":"AK","party":"dem","total":406,"locals":

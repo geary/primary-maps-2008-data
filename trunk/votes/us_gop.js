334151eeb29ce334b9f2ad9df92d7471d20fc805
election.maps.2008 <election.maps.2008@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-06-04 09:12:26 +0000

    Vote update

--- trunk/votes/us_gop.js
+++ trunk/votes/us_gop.js
@@ -1,9 +1,9 @@
 GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":[
-{"votes":9501507,"name":"mccain","delegates":1225},
+{"votes":9501367,"name":"mccain","delegates":1225},
 {"votes":4375380,"name":"romney","delegates":271},
 {"votes":4147799,"name":"huckabee","delegates":273},
-{"votes":1164431,"name":"paul","delegates":19},
+{"votes":1164386,"name":"paul","delegates":19},
 {"votes":573763,"name":"giuliani"},
 {"votes":285898,"name":"thompson"},
 {"votes":67018,"name":"uncommitted-r"},
@@ -20,7 +20,7 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 {"votes":23,"name":"fendig"}],
 "precincts":
 {"total":0,"reporting":0}},
-"state":"US","party":"gop","total":20244338,"locals":
+"state":"US","party":"gop","total":20244153,"locals":
 {"Mississippi":
 {"votes":[
 {"votes":113074,"name":"mccain","delegates":36},
@@ -97,8 +97,8 @@ GoogleElectionMap.votesReady({"status":"ok","totals":
 "delegates":31,"date":"02-05"},
 "New Mexico":
 {"votes":[
-{"votes":93636,"name":"mccain"},
-{"votes":15341,"name":"paul"}],
+{"votes":93496,"name":"mccain"},
+{"votes":15296,"name":"paul"}],
 "name":"New Mexico","precincts":
 {"total":1633,"reporting":1631},
 "delegates":32,"date":"06-03"},

5c4e5b99c1754ad9a2a12bdf6b396651d81a89fa
election.maps.2008@gmail.com <election.maps.2008@gmail.com@4d23cfce-6745-0410-984e-7d6ad6b324a0>
2008-06-14 21:58:20 +0000

    Placeholder tweets

---
+++ tweets/tweets-later.js
@@ -0,0 +1,1 @@
+[{"user":"google","name":"google","lon":"-95.677068","where":"","status":0,"message":"Twitter updates coming later today!","time":1213166259,"author":"google","image":"","lat":"37.0625"}]
